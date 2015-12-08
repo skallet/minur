8628f16e48e46656ef4538746f02bd6113399376
Milan Blažek <Milan Blaek>
2015-12-08 23:59:03 +0100

    Comment form type.

---
+++ src/PalufTeamBundle/FormData/CommentData.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PalufTeamBundle\FormData;
+
+use Symfony\Component\Validator\Constraint as Assert;
+
+/**
+ * Created by PhpStorm.
+ * User: blaze
+ * Date: 08.12.2015
+ * Time: 23:53
+ */
+class CommentData
+{
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    public $text;
+
+}
