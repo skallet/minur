6c82bd2f7100be825e9c79bc82418cd538c998a0
Marketa Zadrazilova <m.zadrazilova@gmail.com>
2015-12-15 18:12:03 +0100

    Issue #4 - update of labels

--- src/PalufTeamBundle/Form/TermType.php
+++ src/PalufTeamBundle/Form/TermType.php
@@ -5,6 +5,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 class TermType extends AbstractType
 {
@@ -12,11 +15,11 @@ class TermType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('start')
-            ->add('end')
-            ->add('place')
-            ->add('gps', NULL, ["required" => false])
-            ->add('count')
+            ->add('start', NULL, ['label' => 'Od'])
+            ->add('end', NULL, ['label' => 'Do'])
+            ->add('place', TextType::class, ['label' => 'Místo'])
+            ->add('gps', TextType::class, ["required" => false, 'label' => 'GPS souřadnice'])
+            ->add('count', IntegerType::class, ['label' => 'Počet hráčů'])
         ;
     }
 
